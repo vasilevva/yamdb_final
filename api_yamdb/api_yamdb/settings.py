27a20b07a3f0d47f84aa570fa849aefd2a0bb2eb
Vladimir Vasilev <vasilevvladv@yandex.ru>
2022-06-23 01:32:23 +0400

    Debug

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,9 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default="p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
+    'web',
     '51.250.98.97',
     '127.0.0.1',
     'tisch.hopto.org'
